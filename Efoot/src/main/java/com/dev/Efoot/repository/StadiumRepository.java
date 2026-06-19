916e375e46a0d3f5d4443f6bbc8969f9bc7d56df
Nicolas Belmonte <nico.belmonte2014@gmail.com>
2026-06-19 11:56:25 -0300

    feat: criando o repository do projeto

---
+++ Efoot/src/main/java/com/dev/Efoot/repository/StadiumRepository.java
@@ -0,0 +1,7 @@
+package com.dev.Efoot.repository;
+
+import com.dev.Efoot.entity.Stadium;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StadiumRepository extends JpaRepository<Stadium, Long> {
+}
