f9f6345af181f77bf9f8016aeaccf369e41e8a91
Nicolas Belmonte <nico.belmonte2014@gmail.com>
2026-06-19 11:49:41 -0300

    Initial commit

---
+++ Efoot/src/main/java/com/dev/Efoot/EfootApplication.java
@@ -0,0 +1,13 @@
+package com.dev.Efoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfootApplication.class, args);
+	}
+
+}
---
+++ Efoot/src/test/java/com/dev/Efoot/EfootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.Efoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
