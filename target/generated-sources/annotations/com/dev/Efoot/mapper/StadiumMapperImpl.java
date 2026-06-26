877ea4173d74aba8a8972f774eae11b317f31461
Nicolas Belmonte <nico.belmonte2014@gmail.com>
2026-06-26 11:57:11 -0300

    fix: ajustando dependencias que estavam erradas

--- target/generated-sources/annotations/com/dev/Efoot/mapper/StadiumMapperImpl.java
+++ target/generated-sources/annotations/com/dev/Efoot/mapper/StadiumMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-06-23T11:15:56-0300",
+    date = "2026-06-26T11:41:03-0300",
     comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.16 (Microsoft)"
 )
 @Component
