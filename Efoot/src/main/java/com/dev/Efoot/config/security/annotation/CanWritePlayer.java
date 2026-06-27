2d1aa8392f990dc1ebcc940b6150a6d3a5e523ba
Nicolas Belmonte <nico.belmonte2014@gmail.com>
2026-06-27 10:44:34 -0300

    feat: criando a annotation de ler e escrever player

---
+++ Efoot/src/main/java/com/dev/Efoot/config/security/annotation/CanWritePlayer.java
@@ -0,0 +1,14 @@
+package com.dev.Efoot.config.security.annotation;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasAnyAuthority('SCOPE_admin:all', 'SCOPE_player:write')")
+public @interface CanWritePlayer {
+}
